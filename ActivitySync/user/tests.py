c0c67ecfa18e515e4b3db02812f283f9bfe6bf03
Michal Wolny <michalwoln@gmail.com>
2016-07-04 15:40:05 +0200

    testing travis  - add test

--- ActivitySync/user/tests.py
+++ ActivitySync/user/tests.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
 # Create your tests here.
+from ActivitySync.user.models import UserProfile
+
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        u = User.objects.create_user('adam')
+        u.profile = UserProfile.objects.create()
+
+    def test_user_profile_defaults_set_properly(self):
+        u = User.objects.get(username='adam')
+        self.assertEqual(0,u.profile.acknowledged)
